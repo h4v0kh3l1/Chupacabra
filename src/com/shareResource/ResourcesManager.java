2ec8e4a9202c2a697c170d2a9e628aa833bfca56
hwang4 <heli.h101@gmail.com>
2012-07-19 15:34:54 -0700

    Property changes and beginning add sharingResources

---
+++ src/com/shareResource/ResourcesManager.java
@@ -0,0 +1,11 @@
+package com.shareResource;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hwang4
+ * Date: 7/19/12
+ * Time: 3:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ResourcesManager {
+}
